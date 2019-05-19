2e728ee331cbcb1c428e66415160d5bb874e741a
Alexandr Chizhkov <chizhkov422@gmail.com>
2019-05-19 14:36:57 +0300

    Refactor add/remove method

--- src/containers/RangesCont.js
+++ src/containers/RangesCont.js
@@ -11,35 +11,116 @@ class RangesCont extends React.Component {
     let newArray = this.state.list;
     let counter = 0;
 
-    for (let i = inputArray[0], l = inputArray[1]; i <= l; i++) {
-      if (!newArray.includes(i)) {
-        newArray.push(i);
-        counter++;
+    if (newArray.length) {
+      for (let i = 0, l = newArray.length; i < l; i++) {
+        if (newArray[i][1] < inputArray[0] - 1 || newArray[i][0] > inputArray[1] + 1) {
+          continue;
+        }
+        if (newArray[i][0] < inputArray[0] && newArray[i][1] > inputArray[1]) {
+          counter++;
+
+          continue;
+        }
+        if ((newArray[i][0] <= inputArray[0] && newArray[i][1] < inputArray[1]) || (newArray[i][1] + 1 === inputArray[0])) {
+          newArray[i][1] = inputArray[1];
+          counter++;
+
+          continue;
+        }
+        if ((newArray[i][0] > inputArray[0] && newArray[i][1] >= inputArray[1]) || (newArray[i][0] - 1 === inputArray[1])) {
+          newArray[i][0] = inputArray[0];
+          counter++;
+
+          continue;
+        }
+        if (newArray[i][0] > inputArray[0] && newArray[i][1] < inputArray[1]) {
+          newArray[i][0] = inputArray[0];
+          newArray[i][1] = inputArray[1];
+          counter++;
+
+          continue;
+        }
       }
     }
 
-    if (counter) {
-      newArray.sort((a, b) => a - b);
-
-      this.setListView(newArray);
-      await this.setState({ list: newArray, cleaningField: true });
-    } else {
-      await this.setState({ cleaningField: false });
-      alert('Gap already added previously');
+    if (!counter) {
+      newArray.push(inputArray);
     }
+
+    if (newArray.length > 1) {
+      newArray.sort((a, b) => {
+        return a[0] - b[0];
+      });
+
+      newArray.reduce((prevValue, currValue, index) => {
+        if (prevValue[1] >= currValue[0]) {
+          prevValue[1] = currValue[1];
+
+          newArray.splice(index, 1);
+        }
+
+        return currValue;
+      });
+    }
+
+    this.setListView(newArray);
+    this.setState({ list: newArray });
   }
   removeRange = async (inputArray) => {
+
     let newArray = this.state.list;
+    let indexesToRemove = [];
     let counter = 0;
 
-    for (let i = inputArray[0], l = inputArray[1]; i <= l; i++) {
-      let index = newArray.indexOf(i);
+    if (newArray.length) {
+      for (let i = 0, l = newArray.length; i < l; i++) {
+        if (!newArray.length) {
+          break;
+        }
+        if (!newArray[i] || newArray[i][1] < inputArray[0] - 1 || newArray[i][0] > inputArray[1] + 1) {
+          continue;
+        }
+        if (newArray[i][0] < inputArray[0] && newArray[i][1] > inputArray[1]) {
+          const newGap = [inputArray[1] + 1, newArray[i][1]];
 
-      if (index !== -1) {
-        newArray.splice(index, 1);
-        counter++;
+          newArray.push(newGap);
+          newArray[i][1] = inputArray[0] - 1;
+          counter++;
+
+          continue;
+        }
+        if (newArray[i][0] <= inputArray[0]) {
+          newArray[i][0] === inputArray[0] ? indexesToRemove.push(i) : newArray[i][1] = inputArray[0] - 1;
+          counter++;
+
+          continue;
+        }
+        if (newArray[i][1] >= inputArray[1]) {
+          newArray[i][1] === inputArray[1] ? indexesToRemove.push(i) : newArray[i][0] = inputArray[1] + 1;
+          counter++;
+
+          continue;
+        }
+        if (newArray[i][0] > inputArray[0] && newArray[i][1] < inputArray[1]) {
+          indexesToRemove.push(i);
+          counter++;
+
+          continue;
+        }
+      }
+    } else {
+      alert('There are no values in the list');
+    }
+
+    if (indexesToRemove.length) {
+      for (let i = indexesToRemove.length - 1, l = 0; i >= l; i--) {
+        newArray.splice(indexesToRemove[i], 1);
       }
     }
+
+    newArray.sort((a, b) => {
+      return a[0] - b[0];
+    });
 
     if (counter) {
       this.setListView(newArray);
@@ -50,23 +131,10 @@ class RangesCont extends React.Component {
     }
   }
   setListView(newArray) {
-    let startGap = [newArray[0]];
-    let endGap = [];
     let resultString = '';
 
-    newArray.reduce((prevValue, currValue) => {
-      if ((currValue - prevValue) > 1) {
-        startGap.push(currValue);
-        endGap.push(prevValue);
-      }
-
-      return currValue;
-    });
-
-    endGap.push(newArray[newArray.length - 1]);
-
-    for (let i = 0, l = startGap.length; i < l; i++) {
-      resultString += `[${startGap[i]}, ${endGap[i]}] `;
+    for (let i = 0, l = newArray.length; i < l; i++) {
+      resultString += `[${newArray[i]}] `;
     }
 
     this.setState({ listView: resultString });
